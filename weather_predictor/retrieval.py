35d093d727b2b63048f1ba97c5cd71689ba27b90
Andreas <seidelandreas001@gmail.com>
2025-08-28 20:43:56 +0200

    updates

--- weather_predictor/retrieval.py
+++ weather_predictor/retrieval.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 import requests
+from meteostat import Point, Daily, Hourly
+from meteostat import Stations
+
 
 
 load_dotenv()
@@ -10,30 +13,47 @@ geocoding_key = os.getenv("GEOCODING_API_KEY")
 
 base_url = "http://api.weatherapi.com/v1"
 
-def get_current_weather(location, key = weatherapi_key):
-    
-    url = f"{base_url}/current.json?key={key}&q={location}"
-    
-    response = requests.get(url)
-    
-    data = response.json()
-    
-    return data
 
 
-def get_coordinates(location):
+class Location:
     
-    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={geocoding_key}"
+    def __init__(self, location):
+        self.location = location
+        self.lat, self.lng = self.get_coordinates()
+        
 
-    response = requests.get(url)
+    def get_current_weather(self, key = weatherapi_key):
     
-    data = response.json()
+        url = f"{base_url}/current.json?key={key}&q={self.location}"
+        
+        response = requests.get(url)
+        
+        data = response.json()
+        
+        return data
+
+    def get_coordinates(self):
     
-    lat = round(data["results"][0]["geometry"]["location"]["lat"], ndigits= 4)
-    lng = round(data["results"][0]["geometry"]["location"]["lng"], ndigits = 4)
+        url = f"https://maps.googleapis.com/maps/api/geocode/json?address={self.location}&key={geocoding_key}"
+
+        response = requests.get(url)
+        
+        data = response.json()
+        
+        lat = round(data["results"][0]["geometry"]["location"]["lat"], ndigits= 4)
+        lng = round(data["results"][0]["geometry"]["location"]["lng"], ndigits = 4)
+        
+        return lat, lng
     
-    return lat, lng
-    
+    def fetch_station(self):
+        
+        stations = Stations()
+        stations = stations.nearby(self.lat, self.lng)
+        station = stations.fetch(1)
+
+        return station
+            
+        
     
 #lat, lng = get_coordinates("Calw")
 #print(f"lat {lat}, lon {lng}")
